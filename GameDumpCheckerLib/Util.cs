7fdf2cd5dc675662dd95262091900b2aaf78fd12
Admiral H. Curtiss <pikachu025@gmail.com>
2017-07-30 12:49:58 +0200

    Add utility function to print a byte array as human-readable hexadecimal string.

---
+++ GameDumpCheckerLib/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDumpCheckerLib {
+    public static class Util {
+        public static string ByteArrayToHumanReadableHexString( byte[] data ) {
+            StringBuilder sb = new StringBuilder( data.Length * 2 );
+            foreach ( byte b in data ) {
+                sb.AppendFormat( "{0:X2}", b );
+            }
+            return sb.ToString();
+        }
+    }
+}
