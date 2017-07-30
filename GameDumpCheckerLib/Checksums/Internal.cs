1b19b26684ae9c6f3c5f9020fa4a069e41e9d083
Admiral H. Curtiss <pikachu025@gmail.com>
2017-07-30 14:18:25 +0200

    Convenience methods for MD5, SHA1, SHA256, and SHA512.

---
+++ GameDumpCheckerLib/Checksums/Internal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDumpCheckerLib.Checksums {
+    public static class Internal {
+        public static byte[] CalculateChecksum( Stream s, HashAlgorithm algorithm ) {
+            long pos = s.Position;
+            s.Position = 0;
+            byte[] crc32 = algorithm.ComputeHash( s );
+            s.Position = pos;
+            return crc32;
+        }
+
+        public static byte[] CalculateMD5( Stream s ) {
+            using ( var algorithm = System.Security.Cryptography.MD5.Create() ) {
+                return CalculateChecksum( s, algorithm );
+            }
+        }
+
+        public static byte[] CalculateSHA1( Stream s ) {
+            using ( var algorithm = System.Security.Cryptography.SHA1.Create() ) {
+                return CalculateChecksum( s, algorithm );
+            }
+        }
+
+        public static byte[] CalculateSHA256( Stream s ) {
+            using ( var algorithm = System.Security.Cryptography.SHA256.Create() ) {
+                return CalculateChecksum( s, algorithm );
+            }
+        }
+
+        public static byte[] CalculateSHA512( Stream s ) {
+            using ( var algorithm = System.Security.Cryptography.SHA512.Create() ) {
+                return CalculateChecksum( s, algorithm );
+            }
+        }
+    }
+}
