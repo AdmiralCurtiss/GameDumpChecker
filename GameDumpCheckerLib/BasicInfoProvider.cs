b5b401ada5cbf6a3e77d092d4985be62565f9c01
Admiral H. Curtiss <pikachu025@gmail.com>
2017-07-30 15:29:59 +0200

    Provide some basic file info (name, size, checksums).

---
+++ GameDumpCheckerLib/BasicInfoProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDumpCheckerLib {
+    public static class BasicInfoProvider {
+        public static DataSection GetDataForFile( string filename ) {
+            var data = new List<(string Key, string Value)>();
+
+            using ( Stream s = new System.IO.FileStream( filename, FileMode.Open ) ) {
+                data.Add( ("Filename", System.IO.Path.GetFileName( filename )) );
+                data.Add( ("Filesize", s.Length + " bytes") );
+                data.Add( ("CRC32", Util.ByteArrayToHumanReadableHexString( Checksums.CRC32.CalculateCRC32( s ) )) );
+                data.Add( ("MD5", Util.ByteArrayToHumanReadableHexString( Checksums.Internal.CalculateMD5( s ) )) );
+                data.Add( ("SHA1", Util.ByteArrayToHumanReadableHexString( Checksums.Internal.CalculateSHA1( s ) )) );
+                data.Add( ("SHA256", Util.ByteArrayToHumanReadableHexString( Checksums.Internal.CalculateSHA256( s ) )) );
+                data.Add( ("SHA512", Util.ByteArrayToHumanReadableHexString( Checksums.Internal.CalculateSHA512( s ) )) );
+            }
+
+            return new DataSection( "Basic Info", data );
+        }
+    }
+}
---
+++ GameDumpCheckerLib/DataSection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDumpCheckerLib {
+    public class DataSection {
+        public string Name { get; private set; }
+        public List<(string Key, string Value)> Data { get; private set; }
+
+        public DataSection( string name, List<(string Key, string Value)> data ) {
+            this.Name = name;
+            this.Data = data;
+        }
+    }
+}
