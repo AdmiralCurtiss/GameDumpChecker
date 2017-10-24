606bcb85201e4ae0f15c34990ea0e08665369f1a
Admiral H. Curtiss <pikachu025@gmail.com>
2017-10-25 00:23:03 +0200

    Log checksum calculations since they can take a bit.

--- GameDumpCheckerLib/FileInfoProvider.cs
+++ GameDumpCheckerLib/FileInfoProvider.cs
@@ -13,10 +13,15 @@ namespace GameDumpCheckerLib {
             using ( Stream s = new System.IO.FileStream( filename, FileMode.Open ) ) {
                 data.Add( ("Filename", System.IO.Path.GetFileName( filename )) );
                 data.Add( ("Filesize", s.Length + " bytes") );
+                Console.WriteLine( "Calculating CRC32 of " + filename );
                 data.Add( ("CRC32", Util.ByteArrayToHumanReadableHexString( Checksums.CRC32.CalculateCRC32( s ) )) );
+                Console.WriteLine( "Calculating MD5 of " + filename );
                 data.Add( ("MD5", Util.ByteArrayToHumanReadableHexString( Checksums.Internal.CalculateMD5( s ) )) );
+                Console.WriteLine( "Calculating SHA1 of " + filename );
                 data.Add( ("SHA1", Util.ByteArrayToHumanReadableHexString( Checksums.Internal.CalculateSHA1( s ) )) );
+                Console.WriteLine( "Calculating SHA256 of " + filename );
                 data.Add( ("SHA256", Util.ByteArrayToHumanReadableHexString( Checksums.Internal.CalculateSHA256( s ) )) );
+                Console.WriteLine( "Calculating SHA512 of " + filename );
                 data.Add( ("SHA512", Util.ByteArrayToHumanReadableHexString( Checksums.Internal.CalculateSHA512( s ) )) );
             }
 
