65bd63a6c7e7f4d1e8bafc00cdab59f13064869a
Admiral H. Curtiss <pikachu025@gmail.com>
2017-10-24 23:45:16 +0200

    Simple command line interface.

--- GameDumpCheckerCli/Program.cs
+++ GameDumpCheckerCli/Program.cs
@@ -7,19 +7,67 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GameDumpCheckerCli {
+    class Config {
+        public FileType FileType;
+    }
+
     class Program {
-        static void Main( string[] args ) {
-            foreach ( string arg in args ) {
-                System.IO.File.WriteAllText( arg + ".info.txt", GetInfoAsString( arg ) );
+        static int Main( string[] args ) {
+            List<string> freeArgs = new List<string>();
+            Config config = new Config();
+            {
+                Queue<string> remainingArgs = new Queue<string>( args );
+                try {
+                    while ( remainingArgs.Count > 0 ) {
+                        string arg = remainingArgs.Dequeue();
+                        switch ( arg ) {
+                            case "--":
+                                freeArgs.AddRange( remainingArgs );
+                                remainingArgs.Clear();
+                                break;
+                            case "--type":
+                                config.FileType = (FileType)Enum.Parse( typeof( FileType ), remainingArgs.Dequeue(), true );
+                                break;
+                            default:
+                                freeArgs.Add( arg );
+                                break;
+                        }
+                    }
+                } catch ( Exception ) {
+                    PrintUsage();
+                    return -1;
+                }
             }
+
+            if ( freeArgs.Count == 0 ) {
+                PrintUsage();
+                return -1;
+            }
+
+            foreach ( string arg in freeArgs ) {
+                System.IO.File.WriteAllText( arg + ".info.txt", GetInfoAsString( config, arg ) );
+            }
+
+            return 0;
         }
 
-        static string GetInfoAsString( string filename ) {
+        static void PrintUsage() {
+            Console.WriteLine( "GameDumpChecker [options] file [file] [...]" );
+            Console.WriteLine();
+            Console.WriteLine( "Options:" );
+            Console.WriteLine( " --type psp/gcn" );
+            Console.WriteLine();
+        }
+
+        static string GetInfoAsString( Config config, string filename ) {
             StringBuilder sb = new StringBuilder();
 
             List<DataSection> dataSections = new List<DataSection>();
             dataSections.Add( FileInfoProvider.GetDataForFile( filename ) );
-            dataSections.AddRange( new PspGameReader( filename ).GenerateSystemSpecificDataSections() );
+            Console.WriteLine( "Parsing game data of " + filename + " with type " + config.FileType );
+            IGameReader reader = GameReaderFactory.ConstructFromType( config.FileType, filename );
+            dataSections.AddRange( reader.GenerateManuallyFilledInfoSections() );
+            dataSections.AddRange( reader.GenerateSystemSpecificDataSections() );
 
             int keymax = 0;
             int valuemax = 0;
---
+++ GameDumpCheckerLib/Readers/GameReaderFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDumpCheckerLib.Readers {
+    public static class GameReaderFactory {
+        public static IGameReader ConstructFromType( FileType type, string filename ) {
+            switch ( type ) {
+                case FileType.PSP:
+                    return new PspGameReader( filename );
+                case FileType.GCN:
+                    return new GamecubeGameReader( filename );
+            }
+
+            throw new Exception( "Invalid FileType: " + type );
+        }
+    }
+}
