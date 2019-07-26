ab71acfaa9b8795202ac4a3bbc228713014ed003
Admiral H. Curtiss <pikachu025@gmail.com>
2019-07-26 21:33:45 +0200

    Rewrite ExHeaderReader with EncryptedStream.

--- GameDumpCheckerLib/N3DS/ExHeaderReader.cs
+++ GameDumpCheckerLib/N3DS/ExHeaderReader.cs
@@ -5,23 +5,21 @@ using HyoutaUtils.Streams;
 
 namespace GameDumpCheckerLib.N3DS {
 	public class ExHeaderReader {
-		public DuplicatableStream Stream;
+		public DuplicatableStream DecryptedStream;
 		public string Title;
 		public ulong Flags;
 
 		public ExHeaderReader( DuplicatableStream stream, NcsdReader ncsd, NcchReader ncch, KeyProvider keys, NcchReader.EncryptionType encryption, byte[] counter ) {
-			Stream = stream.Duplicate();
-
-			var data = new byte[0x800];
-			stream.Read( data, 0, data.Length );
-
+			DuplicatableStream s = stream;
 			if ( encryption != NcchReader.EncryptionType.None ) {
-				data = Encryption.Decrypt( data, ncch.Key0, counter );
+				s = new EncryptedStream( s, ncch.Key0, counter );
 			}
 
 			// there's a lot more in here but we don't really care about the rest
-			Title = new DuplicatableByteArrayStream( data ).ReadAscii( 8 ).TrimNull();
-			Flags = BitConverter.ToUInt64( data, 0x8 );
+			Title = s.ReadAscii( 8 ).TrimNull();
+			Flags = s.ReadUInt64();
+
+			DecryptedStream = s;
 		}
 	}
 }
